406fdd4b48a27360ea4a06afa4640e4799e4f1d9
vk0293 <vk0293@gmail.com>
2015-08-20 08:33:02 +0530

    this is the updated file

--- calci.cpp
+++ calci.cpp
@@ -1,3 +1,4 @@
-I am Varada Karandikar
-I am ib computer branch
-I go to Cummins college of engineering for women
+I am Varada Karandikar.
+I am in computer branch.
+I go to Cummins college of engineering for women.
+I am from Pune Maharashtra.
